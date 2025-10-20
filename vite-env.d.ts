4ef8b2e34c540fe5426f9098488eceed9d06af9f
appprolabs-coder <appprolabs@gmail.com>
2025-10-20 16:16:27 -0400

    Remove Vite configuration from vite-env.d.ts
    Removed Vite configuration from vite-env.d.ts.

--- vite-env.d.ts
+++ vite-env.d.ts
@@ -1,8 +1,2 @@
 /// <reference types="vite/client" />
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  base: "/", // or "/appprolabs-coder.github.io/" if it's not your user root
-  plugins: [react()],
-});
